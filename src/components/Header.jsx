8b43c73d6da489fe43eb716298c24768be43225f
Jonelle Cubio <girlthatloveswriting@gmail.com>
2025-08-17 17:54:47 +0800

    ✈️ Added the header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,6 +3,7 @@ import ico from "../assets/ico.png";
 export default function Header() {
   return(
     <main className="header-main">
+
       <div className="header-brand-con">
         <img src={ico} alt="Wanderplan" className="header-brand-ico" />
         <div className="header-brand-name">Wanderplan</div>
@@ -11,13 +12,13 @@ export default function Header() {
       <div className="header-space"></div>
 
       <div className="header-nav-con">
-        <li>
+        <div>
           <div className="header-nav-li">Home</div>
           <div className="header-nav-li">Trip Planner</div>
           <div className="header-nav-li">Budget</div>
           <div className="header-nav-li">Map</div>
           <div className="header-nav-li">Settings</div>
-        </li>
+        </div>
       </div>
     </main>
   );
