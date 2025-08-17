699fa3f79fb51aafb453807e462189998b84f946
Jonelle Cubio <girlthatloveswriting@gmail.com>
2025-08-17 18:10:01 +0800

    ✈️ Added the Hero text

---
+++ src/components/Hero.jsx
@@ -0,0 +1,12 @@
+export default function Hero() {
+
+  console.log('Hero loaded!')
+  return(
+    <main className="hero-main">
+      <div className="hero-title-con">
+        <div className="hero-title">Plan Your Perfect Journey</div>
+        <div className="hero-text">Organize trips, track budgets, and explore destinations all in one place. <br></br>Start planning your next adventure today.</div>
+      </div>
+    </main>
+  );
+}
