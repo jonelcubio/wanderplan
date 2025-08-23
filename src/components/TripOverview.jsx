62b82c37057f2744c6fbb0b9343ade250ab69f2b
Jonelle Cubio <girlthatloveswriting@gmail.com>
2025-08-23 15:57:31 +0800

    ✈️ Added the trip overview component inputs.

--- src/components/TripOverview.jsx
+++ src/components/TripOverview.jsx
@@ -6,8 +6,22 @@ export default function TripOverview() {
 
       <div className="trip-overview-con">
         <div className="trip-trip-title">Trip Title:</div>
-        <input type="text" name="trip-title" id="trip-title" className="trip-trip-input" placeholder="My Trip Title"/>
-        </div>
+        <input type="text" name="trip-title" id="trip-title" className="trip-trip-input" placeholder="Jonelle's First Vacation"/>
+      </div>
+
+
+      <div className="trip-overview-con">
+        <div className="trip-destination">Destination:</div>
+        <input type="text" name="trip-destination" id="trip-destination" className="trip-destination-input" placeholder="Philippines"/>
+      </div>
+
+      <div className="trip-overview-con">
+        <div className="trip-dates">Dates:</div>
+        <input type="date" name="trip-dates-start" id="trip-dates-start" className="trip-dates-start-input"/>
+        <div className="trip-dates">to</div>
+        <input type="date" name="trip-dates-end" id="trip-dates-end" className="trip-dates-end-input"/>
+      </div>
+
     </main>
   );
 }
