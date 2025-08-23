83696342cadfb8e2c202f1cae5195236c7cb007b
Jonelle Cubio <girlthatloveswriting@gmail.com>
2025-08-23 13:42:03 +0800

    ✈️ Added borders to header and footer, centered footer.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,7 @@
-import ico from "../assets/ico.png";
+
 export default function Footer() {
   return(
     <main className="footer-main">
-      <div className="footer-brand-con">
-        <img src={ico} alt="Wanderplan" className="footer-brand-ico" />
-        <div className="footer-brand-title">Wanderplan</div>
-      </div>
-
-      <div className="footer-space"></div>
 
       <div className="footer-nav-notes">Made with ❤️ & 🪲by <a href="https://github.com/jonelcubio/wanderplan">Jonel Cubio</a></div>
     </main>
